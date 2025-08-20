0a114e5674a72c60e036f6a0e4fa9357d19f5028
Tu <yisehaktursina@gmail.com>
2025-08-20 13:34:16 +0300

    Task 2: connect Express server with MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
+// LOAD ENV VARIABLES
 require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const projectRoutes = require("./routes/projectRoutes");
 
@@ -13,14 +16,19 @@ app.use(express.json());
 // ROUTES
 app.use("/api/projects", projectRoutes);
 
-// ROOT ENDPOINT (OPTIONAL – FOR TESTING)
+
 app.get("/", (req, res) => {
-  res.send("BOOTCAMP PROJECT TRACKER API IS RUNNING 🚀");
+  res.send("✅ BOOTCAMP PROJECT TRACKER BACKEND IS RUNNING");
 });
+
+// MONGO CONNECTION
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MONGO DB CONNECTED"))
+  .catch((err) => console.error("❌ MONGO CONNECTION FAILED:", err));
 
 const PORT = process.env.PORT || 3000;
-const APP_NAME = process.env.APP_NAME || "APP";
+const APP_NAME = process.env.APP_NAME || "App";
 
-app.listen(PORT, () => {
-  console.log(`${APP_NAME} IS RUNNING ON PORT ${PORT}`);
-});
+// START SERVER
+app.listen(PORT, () => console.log(`${APP_NAME} RUNNING ON PORT ${PORT}`));
